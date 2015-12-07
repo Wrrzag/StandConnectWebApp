ad42dcdb68fdcb99dd22dbee622eff9cb0ec2681
Marc Piñol <marcpinolpueyo@gmail.com>
2015-12-07 18:25:55 +0100

    Begun REST mappings

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,6 +1,28 @@
 class UrlMappings {
 
 	static mappings = {
+//		"/api/$controller" (controller: "${controller}Rest", action: 'list')
+//		
+//		"/api/event/$id?" (controller: "eventRest", action: 'show') {
+//			"/stand"(controller: "standRest")
+//		}
+		
+		// REST 
+		"/api/events"(resources: "event", includes:['index', 'show'], namespace: "api") {
+			"/stands"		(resources: "stand", includes:['index', 'show'], namespace: "api")
+			"/visitors"		(resources: "visitor", includes:['index', 'show'], namespace: "api")
+			"/businesses"	(resources: "business", includes:['index', 'show'], namespace: "api")
+			"/products"		(resources: "product", includes:['index', 'show'], namespace: "api")
+			"/tags"			(resources: "tag", includes:['index', 'show'], namespace: "api")
+			"/beacons"		(resources: "beacon", includes:['index', 'show'], namespace: "api")
+		}
+		
+		"/api/beacons"(resources: "beacon", includes:['index', 'show'], namespace: "api") {
+			
+		}
+		
+		
+		// DEFAULT
         "/$controller/$action?/$id?(.$format)?"{
             constraints {
                 // apply constraints here
